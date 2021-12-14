a0ffc6543d2c32ac6aeec93cb061bb7451363059
aetlen <57476219+aetlen@users.noreply.github.com>
2021-12-14 14:41:55 +0300

    Добавлена возможность подбирать параметры

--- mainwindow.h
+++ mainwindow.h
@@ -23,8 +23,41 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    couple currentCouple1;
+    couple currentCouple2;
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:  
+
+
+    void set(QString obj, float value);
+
+    float get(QString obj);
+
+    void calculating();
+
+    void  checkSt();
+
+    void setParamMode();
+
+    void setFindParam();
+
+    void calculateRvn();
+
+    void calculateRn();
+
+    void calculateVoltage();
+
+    void calculateCurrent();
+
+    void calculatePower();
+
+    void calculateT();
+
+    void calculateAlpha();
+
+    void setCurrentCouple();
 
 private:
     Ui::MainWindow *ui;
